07b460d58acc8488ab9de4e16fb5c909f806357d
Harish <h.venkataramanappa@talentry.com>
2020-11-14 16:32:30 +0530

    audio test

--- scripts/custom.js
+++ scripts/custom.js
@@ -1,11 +1,12 @@
 var imageUrl = 'http://beta.flockthemes.com/wp-wedding-paradise/rose/wp-content/themes/wedding-paradise/images/heart-snowfall.png';
 $(document).ready(function () {
   $('#snowfall').snowfall({ image: imageUrl, minSize: 10, maxSize: 32 });
-  const audio = document.createElement('audio');
+  // const audio = document.createElement('audio');
 //  audio.autoplay = true;
-  audio.src = 'FinalBGMusic.MP3';
-  audio.load();
-  audio.loop = 'loop';
-  audio.preload = 'auto';
-  console.log(" playing audio ")
+  // audio.src = 'FinalBGMusic.MP3';
+  // audio.load();
+  // audio.loop = 'loop';
+  // audio.preload = 'auto';
+  // console.log(" playing audio ")
+  $("#my_audio").get(0).play();
 });
